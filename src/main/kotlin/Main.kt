418b9d581b7e75452045c8898dab368d6f5d4157
DrCorchit <drcorchit@gmail.com>
2024-04-23 21:57:01 -0400

    Initial Commit.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+import io.javalin.Javalin
+import io.javalin.config.JavalinConfig
+import io.javalin.http.staticfiles.Location
+import java.io.File
+
+fun main() {
+    val app: Javalin = Javalin.create { config: JavalinConfig ->
+        //config.compression.gzipOnly();
+        val f = File("src/main/resources")
+        config.staticFiles.add(f.path, Location.EXTERNAL)
+        config.http.gzipOnlyCompression()
+    }
+
+    app.start(8080)
+}
