02724b339ddab8e51e6dff5d5956f4bbf7d394b7
DrCorchit <drcorchit@gmail.com>
2024-04-24 01:26:09 -0400

    oof 2

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 git pull || exit 1
 echo "Updated server code; building  server."
 sudo mvn clean package || exit 1
-sudo cp target/FileServer-*.jar target/server.jar || exit 1
+sudo cp target/AllThatGlitters-*.jar target/server.jar || exit 1
 echo "Starting Server."
 sudo java -jar target/server.jar &
