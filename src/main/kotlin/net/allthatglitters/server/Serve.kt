ae0afdc98d7dece251148fd2477be5769a07153f
DrCorchit <drcorchit@gmail.com>
2024-05-18 01:23:53 -0400

    Debugging start.sh Worked on spell appendix formatting.

--- src/main/kotlin/net/allthatglitters/server/Serve.kt
+++ src/main/kotlin/net/allthatglitters/server/Serve.kt
@@ -3,12 +3,12 @@ package net.allthatglitters.server;
 import io.javalin.Javalin
 import io.javalin.config.JavalinConfig
 import io.javalin.http.staticfiles.Location
-import net.allthatglitters.server.util.HtmlFile
 import net.allthatglitters.server.util.outputDir
 
 object Serve {
     @JvmStatic
     fun main(vararg args: String) {
+        throw Exception("dont start")
         val app: Javalin = Javalin.create { config: JavalinConfig ->
             //config.compression.gzipOnly();
             config.staticFiles.add(outputDir.path, Location.EXTERNAL)
