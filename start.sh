c603cb72cccece8c62e55e5ce23d440b49911699
DrCorchit <drcorchit@gmail.com>
2024-05-30 17:19:30 -0400

    Fixed start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ echo "Updated server code; building  server."
 sudo mvn clean package || exit 1
 sudo cp target/AllThatGlitters-*.jar target/server.jar || exit 1
 echo "Generating Files."
-sudo java -cp target/server.jar net.allthatglitters.server.Generate
+sudo java -cp target/server.jar net.allthatglitters.server.Generator
 echo "Starting Server."
 sudo java -jar target/server.jar $1 &
