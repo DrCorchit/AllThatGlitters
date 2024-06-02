4b2746a1927afa86d21155912f8bf1cebf8bf046
DrCorchit <drcorchit@gmail.com>
2024-06-02 14:55:20 -0400

    Disabled file generation in start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ git pull || exit 1
 echo "Updated server code; building  server."
 sudo mvn clean package || exit 1
 sudo cp target/AllThatGlitters-*.jar target/server.jar || exit 1
-echo "Generating Files."
-sudo java -cp target/server.jar net.allthatglitters.server.Generator || exit 1
+#echo "Generating Files."
+#sudo java -cp target/server.jar net.allthatglitters.server.Generator || exit 1
 echo "Starting Server."
 sudo java -jar target/server.jar $1 &
